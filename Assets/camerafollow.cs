b38ec49d6216a7b214aa7a30e7c71c06de7853ad
Terrence Han <147001387+ThisisTerrencesAc@users.noreply.github.com>
2026-05-19 21:30:08 -0700

    camera follower update
    Camera now uses a SLERP to follow segway

--- Assets/camerafollow.cs
+++ Assets/camerafollow.cs
@@ -1,18 +1,56 @@
+
 using UnityEngine;
 
 public class camerafollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     [SerializeField] private Transform player;
-    void Start()
-    {
+    public Vector2 deadzoneX;
+    public Vector2 deadzoneY;
 
+    private Vector3 initSpeed = new Vector3(0, 1f, 0);
+    private void Start()
+    {
+        //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        deadzoneX = new Vector2(player.position.x - 10f, player.position.x + 10f);
+        deadzoneY = new Vector2(player.position.y - 10f, player.position.y + 10f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        transform.position = player.position;
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + new Vector3(0, 0, -10), ref initSpeed, 0.5f);
+        //     Vector3 targetPos = player.position;
+        //     Vector3 cameraPos = transform.position;
+
+        //     Debug.Log(deadzoneX);
+        //     Debug.Log(deadzoneY);
+
+        //     if (targetPos.x < deadzoneX.x || targetPos.x > deadzoneX.y)
+        //     {
+        //         if (targetPos.x < deadzoneX.x)
+        //         {
+        //             cameraPos.x = targetPos.x - 10f;
+        //         }
+        //         else if (targetPos.x > deadzoneX.y)
+        //         {
+        //             cameraPos.x = targetPos.x + 10f;
+        //         }
+        //         deadzoneX = new Vector2(player.position.x - 20f, player.position.x + 20f);
+        //     }
+
+        //     if (targetPos.y < deadzoneY.x || targetPos.y > deadzoneY.y)
+        //     {
+        //         if (targetPos.y < deadzoneY.x)
+        //         {
+        //             cameraPos.y = targetPos.x - 10f;
+        //         }
+        //         else if (targetPos.y > deadzoneY.y)
+        //         {
+        //             cameraPos.y = targetPos.y + 10f;
+        //         }
+        //         deadzoneY = new Vector2(player.position.y - 20f, player.position.y + 10f);
+        //     }
+
+        //     transform.position = cameraPos;
+        // }
     }
 }
--- Assets/wheel_movement.cs
+++ Assets/wheel_movement.cs
@@ -1,15 +1,11 @@
-using Unity.VisualScripting;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Jobs;
-using UnityEngine.U2D;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class wheel_movement : MonoBehaviour
 {
-    [SerializeField] private float forceStrength = 5f;
-    [SerializeField] private float restoringForce = 5f;
+    [SerializeField] private float forceStrength = 4f;
+    [SerializeField] private float restoringForce = 1.3f;
     [SerializeField] private Rigidbody2D wheelRigidbody;
     [SerializeField] private Transform playerPosition;
     private float direction;
@@ -23,12 +19,12 @@ public class wheel_movement : MonoBehaviour
     {
         if (value.Get<Vector2>().x > 0.01f)
         {
-            direction = 3f;
+            direction = 2f;
         }
 
         else if (value.Get<Vector2>().x < -0.01)
         {
-            direction = -3f;
+            direction = -2f;
         }
 
         else
@@ -49,6 +45,5 @@ public class wheel_movement : MonoBehaviour
 
         Vector3 restoreForce = new Vector3(restore * restoringForce, transform.up.y, 0);
         wheelRigidbody.AddForceAtPosition(restoreForce, playerPosition.position + new Vector3(0, 1f, 0));
-        Debug.Log(restoreForce);
     }
 }
