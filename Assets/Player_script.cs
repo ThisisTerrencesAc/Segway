df15717c3fa496e72603808f832e335a3007d5a3
oysheedey29 <oysheedey29@g.ucla.edu>
2026-05-31 00:06:40 -0700

    Player jump with space bar in tutorial

--- Assets/Player_script.cs
+++ Assets/Player_script.cs
@@ -7,6 +7,8 @@ public class Player_script : MonoBehaviour
     public float movement;
     public Rigidbody2D rb;
     public float speed = 5f;
+    public float jumpHeight = 7f;
+    public bool isGround = true;
     void Start()
     {
         
@@ -22,5 +24,28 @@ public class Player_script : MonoBehaviour
         if (Keyboard.current.rightArrowKey.isPressed)
             movement = 1f;
         rb.linearVelocity = new Vector2(movement * speed, rb.linearVelocity.y);
+        if (Keyboard.current.spaceKey.isPressed && isGround)
+        {
+            Jump();
+            //animator.SetBool("Jump",true);
+            isGround = false;
+        }
+
+    }
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+    }
+        void OnCollisionStay2D(Collision2D other)
+    {   
+        if (other.collider.CompareTag("Ground"))
+            isGround = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Ground"))
+            isGround = false;
     }
 }
+
