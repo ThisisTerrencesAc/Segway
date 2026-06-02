a1829b7d98677e1b1162cdab1692b2d67a944bf4
IndigoPolars <aidango214@gmail.com>
2026-06-01 21:30:31 -0700

    Added win/lose conditions and extra details

---
+++ Assets/WinScreen.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+}
